ea14c234208137e1d3d2e91e6bfb2c879456a56e
M2316 <031pjs@gmail.com>
2024-07-10 01:16:16 +0900

    Back End 임시 해제

--- front-end/src/route/PrivateRoute.jsx
+++ front-end/src/route/PrivateRoute.jsx
@@ -8,24 +8,25 @@ const PrivateRoute = ({ component }) => {
     const {state} = useLocation();
     const [email, setEmail] = useState("");
 
-    const navigate = useNavigate();
+    //BACK END 임시 해제
+    // const navigate = useNavigate();
 
-    const authenticate = localStorage.getItem("userInfo");
-    useEffect(() => {
-        const logoutFlag = state;
-        if ( authenticate) {
-            const tokenDecoded = jwtDecode(authenticate);
-            if(new Date().getTime() > (tokenDecoded.exp*1000)){ // 토큰 만료시간 체크
-                localStorage.removeItem("userInfo") // 만료 토큰으로 요청하면 localStorage에서 제거
-                navigate("/login");
-            }else{
-                setEmail(tokenDecoded.email); // 이메일 셋
-            }
-        } else {
-            localStorage.removeItem("userInfo") // 만료 토큰으로 요청하면 localStorage에서 제거
-            navigate("/login");
-        }
-    }, [state]); 
+    // const authenticate = localStorage.getItem("userInfo");
+    // useEffect(() => {
+    //     const logoutFlag = state;
+    //     if ( authenticate) {
+    //         const tokenDecoded = jwtDecode(authenticate);
+    //         if(new Date().getTime() > (tokenDecoded.exp*1000)){ // 토큰 만료시간 체크
+    //             localStorage.removeItem("userInfo") // 만료 토큰으로 요청하면 localStorage에서 제거
+    //             navigate("/login");
+    //         }else{
+    //             setEmail(tokenDecoded.email); // 이메일 셋
+    //         }
+    //     } else {
+    //         localStorage.removeItem("userInfo") // 만료 토큰으로 요청하면 localStorage에서 제거
+    //         navigate("/login");
+    //     }
+    // }, [state]); 
 
 
     
