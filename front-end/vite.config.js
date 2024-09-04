4ac0252f326a83a1a891e1af802b86a01bb7fdaa
M2316 <031pjs@gmail.com>
2024-09-04 13:47:43 +0900

    배포 테스트 netlify Proxy 수정5

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({mode})=>{
   const isProduction = mode === 'production';
 
 
-  const PROXY = isProduction ? '' :'/proxy'
+  const PROXY = isProduction ? '/proxy' :''
 
 
 
